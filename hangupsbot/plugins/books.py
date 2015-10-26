c75d430fb387d2f420cc57f8eda60ea9b6c3ae86
Abhinav1997 <abhinav.jhanwar.august2@gmail.com>
2015-10-26 21:39:56 +0100

    Add some scripts
    The code is taken from various sources and put together to work.

---
+++ hangupsbot/plugins/books.py
@@ -0,0 +1,42 @@
+import hangups
+
+import plugins
+
+import json
+
+import re
+
+import html
+
+import urllib.request, urllib.parse
+
+def _initialise(bot):
+  plugins.register_user_command(["book"])
+
+def book(bot, event, *args):
+  squery = ' '.join(args).strip()
+  squery = squery.replace(" ", "+")
+  query = re.sub(r'^\.g', u'', squery, re.UNICODE).encode('utf-8')
+  query = query.strip()
+  query = urllib.request.quote(query)
+  url = u'http://ajax.googleapis.com/ajax/services/search/books?v=2.0&q=' + query
+  search_response = urllib.request.urlopen(url)
+  search_results = search_response.read().decode("utf8")
+  results = json.loads(search_results)
+  data = results['responseData']
+  hits = data['results']
+  num = 0
+  for h in hits:
+    while num == 0:
+      title = h['title']
+      url = h['url']
+      num = 1
+  url = urllib.request.unquote(url)
+  title = html.unescape(title)
+
+  yield from bot.coro_send_message(
+      event.conv,
+      _(title))
+  yield from bot.coro_send_message(
+      event.conv,
+      _(url))
