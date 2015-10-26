61ce2b98d1d2c9f84e3bbb78d58d38793e51a87c
Abhinav1997 <abhinav.jhanwar.august2@gmail.com>
2015-10-26 22:00:55 +0100

    Add insults script
    Thanks to https://github.com/Shardvex/willie-modules/blob/master/insult.py and http://randominsults.net/
    
    Signed-off-by: Abhinav1997 <abhinav.jhanwar.august2@gmail.com>

---
+++ hangupsbot/plugins/insult.py
@@ -0,0 +1,22 @@
+import hangups
+
+import plugins
+
+import urllib.request, sys, re, os
+from bs4 import BeautifulSoup
+
+def _initialise(bot):
+  plugins.register_user_command(["insult"])
+    
+def insult(bot, event, *args):
+  name = ' '.join(args).strip().title()
+  insult = []
+  htm = urllib.request.urlopen("http://randominsults.net").read()
+  soup = BeautifulSoup(htm)
+  texts = soup.findAll(text=True)
+  insult = soup.findAll(attrs={'bordercolor' : '#FFFFFF'})[0].getText()
+  insult = os.linesep.join([s for s in insult.splitlines() if s])
+  insult = name + ', ' + insult
+  yield from bot.coro_send_message(
+      event.conv,
+      _(insult))
