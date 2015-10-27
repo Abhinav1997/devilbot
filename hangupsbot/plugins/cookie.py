40b6738c8ce83cc7da528d97ecd6b2d7f53f6012
Abhinav1997 <abhinav.jhanwar.august2@gmail.com>
2015-10-27 14:44:01 +0100

    If no name is provided in argument don't include it
    Signed-off-by: Abhinav1997 <abhinav.jhanwar.august2@gmail.com>

--- hangupsbot/plugins/cookie.py
+++ hangupsbot/plugins/cookie.py
@@ -8,7 +8,10 @@ def _initialise(bot):
 def cookie(bot, event, *args):
   name = ' '.join(args).strip().title()
   symbol =  u"\U0001f36a"
-  cookies = name + ', ' + "have a cookie " + symbol
+  if not name:
+    cookies = "Have a cookie " + symbol
+  else:
+    cookies = name + ', ' + "have a cookie " + symbol
   yield from bot.coro_send_message(
       event.conv,
       _(cookies))
--- hangupsbot/plugins/shakespeare.py
+++ hangupsbot/plugins/shakespeare.py
@@ -14,7 +14,8 @@ def shakespeare(bot, event, *args):
   column2=["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"]
   column3=["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
   insult = "Thou " + column1[random.randint(0,49)] + (" ") + column2[random.randint(1,49)] + (" ") + column3[random.randint(1,49)]
-  insult = name + ', ' + insult
+  if name:
+    insult = name + ', ' + insult
   yield from bot.coro_send_message(
       event.conv,
       _(insult.title()))
