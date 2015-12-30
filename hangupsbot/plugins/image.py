febddfc89c922efccaf415d354fc8783e2080671
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2015-12-30 19:31:51 +0100

    Update image script
    Google deprecated the ajax json method so now we need to use authorize custome search engine
    
    Instructions in python script
    
    config.json entries required:
    cse and image-api
    
    Signed-off-by: Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>

--- hangupsbot/plugins/image.py
+++ hangupsbot/plugins/image.py
@@ -1,61 +1,61 @@
+"""
+instructions:
+* get CSE Search ID from https://cse.google.co.in/cse/
+* put CSE Search ID in config.json:cse
+* more info on https://developers.google.com/custom-search/
+*
+* get image API key from https://console.developers.google.com/apis/
+* put image API key in config.json:image-api
+"""
+
 import hangups
-
 import plugins
-
 import json
-
 import re, io, os
-
 import html, aiohttp
-
 import urllib.request, urllib.parse
 
 def _initialise(bot):
   plugins.register_user_command(["image"])
 
-def image(bot, event, *args):
+def image(bot, event, *args, number=0):
+  try:
+    int(number)
+  except ValueError:
+    number = 0
+  error = 0
   squery = ' '.join(args).strip()
   squery = squery.replace(" ", "+")
   query = re.sub(r'^\.g', u'', squery, re.UNICODE).encode('utf-8')
   query = query.strip()
   query = urllib.request.quote(query)
-  url = u'http://ajax.googleapis.com/ajax/services/search/images?v=2.0&q=' + query
-  search_response = urllib.request.urlopen(url)
-  search_results = search_response.read().decode("utf8")
-  results = json.loads(search_results)
-  data = results['responseData']
-  hits = data['results']
-  num = 0
-  for h in hits:
-    title = h['title']
-    if h['url'].endswith((".jpg", ".gif", "gifv", "png")):
-      while num == 0:
-        url = h['url']
-        num = 1
-  url = urllib.request.unquote(url)
-
+  cse = bot.get_config_option("cse")
+  cse = cse.replace(":","%3A")
+  api = bot.get_config_option("image-api")
+  url = u'https://www.googleapis.com/customsearch/v1?q=' + query + u'&cx=' + cse + u'&key=' + api
   try:
-    title
-  except UnboundLocalError:
-    yield from bot.coro_send_message(
-        event.conv,
-        _("No results found."))
-  else:
-    title = html.unescape(title)
-    if url.endswith((".jpg", ".gif", "gifv", "png")):
-      filename = os.path.basename(url)
-      r = yield from aiohttp.request('get', url)
+    search_response = urllib.request.urlopen(url)
+  except:
+    error = 1
+  if error is 0:
+    search_results = search_response.read().decode("utf8")
+    results = json.loads(search_results)
+    try:
+      data = results['items'][number]['pagemap']['cse_image'][0]['src']
+    except KeyError:
+      data = "placeholder"
+    if data.endswith((".jpg", ".gif", "gifv", "webm", "png", "jpeg")):
+      filename = os.path.basename(data)
+      r = yield from aiohttp.request('get', data)
       raw = yield from r.read()
       image_data = io.BytesIO(raw)
-      try:
-        image_id = yield from bot._client.upload_image(image_data, filename=filename)
-      except KeyError:
-        yield from bot.coro_send_message(
-            event.conv,
-            _("Can't find an image file"))
-        return
+      image_id = yield from bot._client.upload_image(image_data, filename=filename)
       yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
     else:
       yield from bot.coro_send_message(
           event.conv,
-          _("Can't find an image file"))
+          _("There was some problem with image."))
+  else:
+    yield from bot.coro_send_message(
+        event.conv,
+        _("Ran out of today's quota. Try again later."))
