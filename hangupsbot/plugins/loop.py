65013b29e9b39be4f3f49794828fee6b63a80ae9
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2016-01-02 20:08:48 +0100

    Limit the loop command
    Signed-off-by: Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>

--- hangupsbot/plugins/loop.py
+++ hangupsbot/plugins/loop.py
@@ -8,7 +8,9 @@ def loop(bot, event, *args):
   word = ' '.join(args).strip()
   if not word:
     word = "ayy"
-  while True:
+  count = 50
+  while count > 0:
     yield from bot.coro_send_message(
         event.conv,
         _(word))
+    count = count - 1
