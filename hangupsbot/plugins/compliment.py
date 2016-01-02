b23295d6fb796205c798155a2b7947e28966c9ee
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2016-01-02 19:51:37 +0100

    Use different method for compliment
    The previous compliments have been terrible.
    New compliments taken from: http://project-positive.org/complimentmachine.html
    
    Signed-off-by: Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>

--- hangupsbot/plugins/compliment.py
+++ hangupsbot/plugins/compliment.py
@@ -1,21 +1,17 @@
 import hangups
-
 import plugins
-
-import urllib.request, sys, re, os
-from bs4 import BeautifulSoup
+import random
+import os
 
 def _initialise(bot):
   plugins.register_user_command(["compliment"])
     
 def compliment(bot, event, *args):
   name = ' '.join(args).strip().title()
-  htm = urllib.request.urlopen("http://www.madsci.org/cgi-bin/cgiwrap/~lynn/jardin/SCG").read()
-  soup = BeautifulSoup(htm, "html5lib")
-  compli = soup.h2.string
-  compli = os.linesep.join([s for s in compli.splitlines() if s])
+  with open(os.path.dirname(__file__) + "/compliments.txt") as f:
+    compliment = f.readlines()
   if name:
-    compli = name + ', ' + compli
+    compliment = name + ', ' + random.choice(compliment)
   yield from bot.coro_send_message(
       event.conv,
-      _(compli))
+      _(compliment.rstrip('\n')))
