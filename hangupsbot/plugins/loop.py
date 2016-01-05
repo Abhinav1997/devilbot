8e4b7f5d1627e90c3a8162155ae7d3f429570c03
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2016-01-05 13:22:24 +0100

    Make loop user command
    Signed-off-by: Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>

--- hangupsbot/plugins/loop.py
+++ hangupsbot/plugins/loop.py
@@ -2,12 +2,12 @@ import hangups
 import plugins
 
 def _initialise(bot):
-  plugins.register_admin_command(["loop"])
+  plugins.register_user_command(["loop"])
 
 def loop(bot, event, *args):
   word = ' '.join(args).strip()
   if not word:
-    word = "ayy"
+    word = "loop"
   count = 25
   while count > 0:
     yield from bot.coro_send_message(
